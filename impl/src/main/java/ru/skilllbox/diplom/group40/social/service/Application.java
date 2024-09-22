69e96741541bbf669e7d336d82a30ae8b75279e7
ivan_kolosov <kolivim@yandex.ru>
2024-09-22 18:26:18 +0300

    5

--- impl/src/main/java/ru/skilllbox/diplom/group40/social/service/Application.java
+++ impl/src/main/java/ru/skilllbox/diplom/group40/social/service/Application.java
@@ -9,3 +9,25 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
+
+/**
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@Profile ("client_app_profile_name")
+public class ClientAppConfiguration {
+    //it can be left blank
+}
+ */
+
+
+/**
+@SpringBootApplication
+public class SomeApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SomeApplication.class);
+    }
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SomeApplication.class);
+    }
+}*/
