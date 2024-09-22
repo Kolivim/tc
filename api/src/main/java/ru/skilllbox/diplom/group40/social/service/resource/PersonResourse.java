2e2dfa1f31c8db958bbfb083ee5e75261222d588
ivan_kolosov <kolivim@yandex.ru>
2024-09-22 18:32:01 +0300

    6 DB delete

--- api/src/main/java/ru/skilllbox/diplom/group40/social/service/resource/PersonResourse.java
+++ api/src/main/java/ru/skilllbox/diplom/group40/social/service/resource/PersonResourse.java
@@ -1,10 +1,7 @@
 package ru.skilllbox.diplom.group40.social.service.resource;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.skilllbox.diplom.group40.social.service.dto.PersonDto;
 import ru.skilllbox.diplom.group40.social.service.dto.drawing.DrawingDto;
 
@@ -13,11 +10,11 @@ import java.util.UUID;
 @RequestMapping("api/v1/test")
 public interface PersonResourse {
 
-    @GetMapping(value = "/drawing")
+    @PostMapping(value = "/drawing")
     void createDrawing(@RequestBody DrawingDto drawingDto);
 //    PersonDto getById(@PathVariable(name = "id") UUID id);
 
-    @GetMapping(value = "/drawing1")
+    @PostMapping(value = "/drawing1")
     void createDrawing1(@RequestBody DrawingDto drawingDto);
 
 }
